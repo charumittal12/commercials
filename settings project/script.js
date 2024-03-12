79d41f6a7d4c300edb4eb0f354ae781f7a95a95c
charumittal12 <117985189+charumittal12@users.noreply.github.com>
2024-03-12 15:06:59 +0530

    commit1

---
+++ settings project/script.js
@@ -0,0 +1,14 @@
+let signup = document.querySelector(".sig");
+let login = document.querySelector(".log");
+let slider = document.querySelector(".login-box");
+let formSection = document.querySelector(".form-section");
+
+signup.addEventListener("click", () => {
+	slider.classList.add("moveslider");
+	formSection.classList.add("form-section-move");
+});
+
+login.addEventListener("click", () => {
+	slider.classList.remove("moveslider");
+	formSection.classList.remove("form-section-move");
+});
