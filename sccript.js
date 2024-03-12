5ee64280e9df7e13de385aabaca7b62294a3df43
charumittal12 <117985189+charumittal12@users.noreply.github.com>
2024-03-12 15:08:28 +0530

    commit2

---
+++ sccript.js
@@ -0,0 +1,71 @@
+//vid1
+let clip1 = document.querySelector(".vid1");
+// Play the video when the mouse hovers over it
+clip1.addEventListener("mouseover", function (e) {
+	clip1.muted=true;
+    clip1.play();
+});
+// Stop the video when the mouse is out of the video
+clip1.addEventListener("mouseout", function (e) {
+    clip1.pause();
+});
+
+//vid2
+let clip2 = document.querySelector(".vid2");
+// Play the video when the mouse hovers over it
+clip2.addEventListener("mouseover", function (e) {
+	clip2.muted=true;
+    clip2.play();
+});
+// Stop the video when the mouse is out of the video
+clip2.addEventListener("mouseout", function (e) {
+    clip2.pause();
+});
+
+//vid3
+let clip3 = document.querySelector(".vid3");
+// Play the video when the mouse hovers over it
+clip3.addEventListener("mouseover", function (e) {
+	clip3.muted=true;
+    clip3.play();
+});
+// Stop the video when the mouse is out of the video
+clip3.addEventListener("mouseout", function (e) {
+    clip3.pause();
+});
+
+//vid4
+let clip4 = document.querySelector(".vid4");
+// Play the video when the mouse hovers over it
+clip4.addEventListener("mouseover", function (e) {
+	clip4.muted=true;
+    clip4.play();
+});
+// Stop the video when the mouse is out of the video
+clip4.addEventListener("mouseout", function (e) {
+    clip4.pause();
+});
+
+//vid5
+let clip5 = document.querySelector(".vid5");
+// Play the video when the mouse hovers over it
+clip5.addEventListener("mouseover", function (e) {
+	clip5.muted=true;
+    clip5.play();
+});
+// Stop the video when the mouse is out of the video
+clip5.addEventListener("mouseout", function (e) {
+    clip5.pause();
+});
+
+//vid6
+let clip6 = document.querySelector(".vid6");
+// Play the video when the mouse hovers over it
+clip6.addEventListener("mouseover", function (e) {
+	clip6.muted=true;
+    clip6.play();
+});
+// Stop the video when the mouse is out of the video
+clip6.addEventListener("mouseout", function (e) {
+    clip6.pause();
+});
